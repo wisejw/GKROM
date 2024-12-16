eebed457339d84b78850a0ccb103accf4caf78ae
wisejw <74333173+wisejw@users.noreply.github.com>
2024-12-16 15:25:27 +0800

    Update train.py

--- train.py
+++ train.py
@@ -30,6 +30,7 @@ def train(args):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print(device)
 
+    # Load data
     data, homogeneous_data = load_data(device)
     r_contain_c_train = pd.read_csv('data/train_test_data/MOOC/r_contain_c_data_train.csv', header=0)
     r_contain_c_val = pd.read_csv('data/train_test_data/MOOC/r_contain_c_data_val.csv', header=0)
@@ -42,12 +43,14 @@ def train(args):
     concept_df = pd.read_csv('data/preprocess_data/MOOC/concepts_index.csv', header=None)
     num_concepts = len(concept_df)
 
+    # Initialize models
     gat = GAT(in_channels=args.in_channels, out_channels1=args.out_channels1, out_channels2=args.out_channels2).to(
         device)
     siamesenet = SiameseNet(input_dim=args.out_channels2).to(device)
     resource_concept_predictNet = ResourceConceptPredictNet(input_dim=args.out_channels2).to(device)
     resource_dependency_predictNet = ResourceDependencyPredictNet(input_dim=args.out_channels2).to(device)
 
+    # Set up optimizer and loss function
     optimizer = torch.optim.Adam(
         list(gat.parameters())
         + list(siamesenet.parameters())
@@ -80,10 +83,12 @@ def train(args):
             optimizer.zero_grad()
             gat_output = gat(homogeneous_data.x.float(), homogeneous_data.edge_index)
 
+            # Calculate main loss
             target = torch.tensor(target).to(device)
             preq_pred_prob = siamesenet(gat_output[c1], gat_output[c2])
             loss1 = criterion(preq_pred_prob, target[:, None].float())
 
+            # Calculate auxiliary loss related to resource-concept containment
             r_contain_c_data = r_contain_c_train[r_contain_c_train['concept'].isin(c1.tolist() + c2.tolist())]
             if len(r_contain_c_data) > args.batch_size:
                 r_contain_c_data = r_contain_c_data.sample(n=args.batch_size, random_state=args.seed)
@@ -104,6 +109,7 @@ def train(args):
             else:
                 loss2 = torch.tensor(0.0, requires_grad=True).to(device)
 
+            # Calculate auxiliary loss related to resource-resource dependency
             if resources_for_aux1:
                 r_depend_on_r_data = r_depend_on_r_train[
                     r_depend_on_r_train['resource_1'].isin(resources_for_aux1)
@@ -125,10 +131,12 @@ def train(args):
             else:
                 loss3 = torch.tensor(0.0, requires_grad=True).to(device)
 
+            # Total loss
             loss = loss1 + args.alpha * loss2 + args.beta * loss3
             total_loss += loss
             loss.backward()
 
+            # Gradient clipping
             torch.nn.utils.clip_grad_norm_(gat.parameters(), max_norm=1.0)
             torch.nn.utils.clip_grad_norm_(siamesenet.parameters(), max_norm=1.0)
             torch.nn.utils.clip_grad_norm_(resource_concept_predictNet.parameters(), max_norm=1.0)
@@ -139,6 +147,7 @@ def train(args):
         average_loss = (total_loss / X_train.shape[0]).float()
         print("epoch: {}, average loss: {}".format(epoch, average_loss.item()))
 
+        # Evaluate on the training data
         train_metrics = evaluate(args, gat, siamesenet, resource_concept_predictNet, resource_dependency_predictNet,
                                  homogeneous_data, train_data, r_contain_c_train, r_depend_on_r_train, num_concepts,
                                  device)
@@ -146,12 +155,14 @@ def train(args):
         print(f'Train metrics (Document-Concept Containment Relation): {train_metrics["resource_concept"]}')
         print(f'Train metrics (Document-Document Dependency Relation): {train_metrics["resource_dependency"]}')
 
+        # Evaluate on the validation data
         val_metrics = evaluate(args, gat, siamesenet, resource_concept_predictNet, resource_dependency_predictNet,
                                homogeneous_data, val_data, r_contain_c_val, r_depend_on_r_val, num_concepts, device)
         print(f'Validation metrics (Concept Prerequisite Relation): {val_metrics["main_task"]}')
         print(f'Validation metrics (Document-Concept Containment Relation): {val_metrics["resource_concept"]}')
         print(f'Validation metrics (Document-Document Dependency Relation): {val_metrics["resource_dependency"]}')
 
+        # Save model
         if val_metrics['main_task']['auc'] > best_val_auc:
             best_val_auc = val_metrics['main_task']['auc']
             model_name = f"GKROM_best_net.pth"
